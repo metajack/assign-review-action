464ae5dab929c3d6912a96afcc9d6c12bf648e64
Jack Moffitt <metajack@fb.com>
2019-09-06 17:18:56 -0500

    wip

--- assign_review/src/main.rs
+++ assign_review/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+//use hubcaps::{Credentials, Github};
+//use jsonpath_lib as jsonpath;
+use std::error::Error;
+use std::{fs, io};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let event_name = std::env::var("GITHUB_EVENT_NAME")?;
+    let event_path = std::env::var("GITHUB_EVENT_PATH")?;
+
+    println!("handling {} event", event_name);
+    println!("reading event data from {}", event_path);
+
+    let file = fs::File::open(event_path)?;
+    let reader = io::BufReader::new(file);
+
+    let data = serde_json::from_reader(reader)?;
+
+    println!("{:#?}", data);
+
+    Ok(())
 }
